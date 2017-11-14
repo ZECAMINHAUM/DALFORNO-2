96e80d5e4aa157d832b7ae01f7a6b75e818d6a29
DAVI ALVES <davi.sousa10@gmail.com>
2017-11-13 22:13:39 -0300

    fazendo o cadastro - testa ai por favorgit add .!

--- main.c
+++ main.c
@@ -13,6 +13,12 @@ main(){
 	
 	setlocale(LC_ALL, "portuguese");
 	
+	struct cadastro{
+		int num_placa=0,ano=0;
+		char carro[20];
+		char marca[50];
+		char placa_letra[5];
+	};
 	char modo;
 	int deci;
 	FILE *arquivo;
@@ -22,18 +28,42 @@ main(){
 	system("cls");
 	
 	deci = menu();	
+
+	if(deci==1){
+		struct cadastro entrada[2];
+		for(int i=0;i<2;i++){
+			scanf("%d",&entrada[i].num_placa);
+			scanf("%s",entrada[i].placa_letra);
+			scanf("%d",&entrada[i].ano);
+			scanf("%s",entrada[i].carro);
+			scanf("%s",entrada[i].marca);
+		}
+		for (int i = 0; i < 2; i++){
+			printf("%d",entrada[i].num_placa);
+			printf("%s",entrada[i].placa_letra);
+			printf("%d",entrada[i].ano);
+			printf("%s",entrada[i].carro);
+			printf("%s",entrada[i].marca);	
+		}
+
+	}
+	
 }
 	
 int menu(){
 	
-	int controle = true, deci;
+	int controle = true, deci=0;
 	do{
-		system ("cls ");
+		
 		printf("O QUE DESEJA FAZER ?\n1 - ENTRAR\n2 - SAIR\nDECISÃO:");
 		scanf("%d", &deci);
 		
-		if ((deci != 1)||(deci != 2)){
-			printf("OPÇÃO INVÁLIDA, POR FAVOR, ESCOLHA UMA OPÇÃO VÁLIDA");
+		if ((deci!=1)||(deci!=2)){
+
+			printf("OPÇÃO VÁLIDA,CONTINUANDO...\n");
+		
+		}else{
+			printf("OPÇÃO INVÁLIDA, POR FAVOR INFORME UMA OPÇÃO VÁLIDA!\n");
 			controle = false;
 		}
 	}while (controle = false);
